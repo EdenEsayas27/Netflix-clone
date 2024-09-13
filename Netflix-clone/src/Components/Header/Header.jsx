30a6ce55ad0032dd54659c2a8c7ed198c06e6894
Eden Esayas Tesema <edenesayas1627@gmail.com>
2024-09-14 02:21:37 +0300

    header responsiveness

--- Netflix-clone/src/Components/Header/Header.jsx
+++ Netflix-clone/src/Components/Header/Header.jsx
@@ -1,46 +1,63 @@
-import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import React, { useEffect, useState } from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import NetflixLogo from '../../assets/NetflixLogo2.png'
-import './Header.css'
+import NetflixLogo from "../../assets/NetflixLogo2.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css";
+
+
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 400);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); // Cleanup
+  }, []);
+
   return (
-    <>
-      <div className="Header_outer_container">
-        <div className="Header_container">
-          <div className="Header_left">
-            <ul>
-              <li><img src={NetflixLogo} alt ="Netflix logo" width="100" /> </li>
-              <li>Home</li>
-              <li>TvShows</li>
-              <li>Movies</li>
-              <li>Latest</li>
-              <li>MyList</li>
-              <li>Browse by Language</li>
-            </ul>
-          </div>
-          <div className="Header_right">
-            <ul>
-              <li>
-                <SearchIcon />
-              </li>
-              <li>
-                <NotificationsIcon />
-              </li>
-              <li>
-                <AccountBoxIcon />
-              </li>
-              <li>
-              < ArrowDropDownIcon />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
+    <Navbar
+      className={`Header_outer_container ${isScrolled ? "nav_black" : ""}`}
+      expand="lg"
+      fixed="top"
+    >
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={NetflixLogo} alt="Netflix logo" width="100" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#tvshows">TV Shows</Nav.Link>
+            <Nav.Link href="#movies">Movies</Nav.Link>
+            <Nav.Link href="#latest">Latest</Nav.Link>
+            <Nav.Link href="#mylist">My List</Nav.Link>
+            <NavDropdown title="Browse by Language" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#language1">Language 1</NavDropdown.Item>
+              <NavDropdown.Item href="#language2">Language 2</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#search">
+              <SearchIcon />
+            </Nav.Link>
+            <Nav.Link href="#notifications">
+              <NotificationsIcon />
+            </Nav.Link>
+            <Nav.Link href="#account">
+              <AccountBoxIcon />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Header
+export default Header;
